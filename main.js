cd7ce670d48a9521900ff0ff85b13eb680a716a2
JaeBotchway <jacqueline.botchway@amalitech.org>
2021-08-10 11:13:36 +0000

    second

---
+++ main.js
@@ -0,0 +1,6 @@
+const Calculator = require('./calculator');
+const calcultor = require('./calculator');
+
+const simpleCalculator = new Calculator(6,7)
+const result = simpleCalculator.add()
+console.log(result)
